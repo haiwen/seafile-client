c4b2e0996ce99956a2c5328bae004e555803ee4e
lins05 <linshuai2012@gmail.com>
2014-04-04 14:49:57 +0800

    imporve ssl confirm dialog

--- src/ui/ssl-confirm-dialog.cpp
+++ src/ui/ssl-confirm-dialog.cpp
@@ -10,28 +10,8 @@ SslConfirmDialog::SslConfirmDialog(const QUrl& url,
 
     setWindowTitle(tr("Untrusted Connection"));
 
-    mIcon->setPixmap(QPixmap(":/images/sync/exclamation.png"));
-
-    QString hint = tr("%1 uses an invalid security certificate. Possible reasons are:").arg(url_.host());
+    QString hint = tr("%1 uses an invalid security certificate. The connection is not secure. Do you want to continue?").arg(url_.host());
     mHint->setText(hint);
-
-    QString error_text;
-    error_text += "<ul>";
-
-    error_text += "<li>";
-    error_text += tr("The server has changed its certificate");
-    error_text += "</li>";
-
-    error_text += "<li>";
-    error_text += tr("You are under security attack");
-    error_text += "</li>";
-
-    error_text += "</ul>";
-
-    mErrorDetail->setText(error_text);
-
-    QString question = tr("Do you still wish to continue?");
-    mQuestion->setText(question);
 
     connect(mYesBtn, SIGNAL(clicked()), this, SLOT(accept()));
     connect(mNoBtn, SIGNAL(clicked()), this, SLOT(reject()));

4378bb2070e2a0f73abfb5663afbd771a909ab9b
Chilledheart <rwindz0@gmail.com>
2014-09-26 11:29:25 +0800

    [utils] add file icons version2

--- src/utils/file-utils.h
+++ src/utils/file-utils.h
@@ -2,6 +2,7 @@
 #define SEAFILE_CLIENT_FILE_UTILS_H_
 
 QString getIconByFileName(const QString& fileName);
+QString getIconByFileNameV2(const QString& fileName);
 
 
 #endif // SEAFILE_CLIENT_FILE_UTILS_H_

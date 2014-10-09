4943cc36da6b597f2d9004d436a2edd51a5957ab
Chilledheart <rwindz0@gmail.com>
2014-10-09 15:22:32 +0800

    [filebrowser] support more APIs now

--- src/filebrowser/file-browser-requests.h
+++ src/filebrowser/file-browser-requests.h
@@ -36,6 +36,70 @@ private:
     QString cached_dir_id_;
 };
 
+class GetDirentSharedLinkRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    GetDirentSharedLinkRequest(const Account &account, const QString &repo_id,
+                               const QString &path);
+
+signals:
+    void success(const QString& url);
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(GetDirentSharedLinkRequest)
+};
+
+class CreateDirentRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    CreateDirentRequest(const Account &account, const QString &repo_id,
+                        const QString &path);
+
+signals:
+    void success(const QString& url);
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(CreateDirentRequest)
+};
+
+class RenameDirentRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    RenameDirentRequest(const Account &account, const QString &repo_id,
+                        const QString &path, const QString &new_path);
+
+signals:
+    void success(const QString& url);
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(RenameDirentRequest)
+};
+
+class RemoveDirentRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    RemoveDirentRequest(const Account &account, const QString &repo_id,
+                        const QString &path);
+
+signals:
+    void success(const QString& url);
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(RemoveDirentRequest)
+};
+
 class GetFileDownloadRequest : public SeafileApiRequest {
     Q_OBJECT
 public:
@@ -52,6 +116,22 @@ private:
     Q_DISABLE_COPY(GetFileDownloadRequest)
 };
 
+class GetFileSharedLinkRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    GetFileSharedLinkRequest(const Account &account, const QString &repo_id,
+                             const QString &path);
+
+signals:
+    void success(const QString& url);
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(GetFileSharedLinkRequest)
+};
+
 class GetFileUploadRequest : public SeafileApiRequest {
     Q_OBJECT
 public:
@@ -66,6 +146,105 @@ protected slots:
 
 private:
     Q_DISABLE_COPY(GetFileUploadRequest)
+};
+
+class GetFileUpdateRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    GetFileUpdateRequest(const Account &account, const QString &repo_id,
+                         const QString &path);
+
+signals:
+    void success(const QString& url);
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(GetFileUpdateRequest)
+};
+
+class RenameFileRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    RenameFileRequest(const Account &account, const QString &repo_id,
+                      const QString &path, const QString &new_name);
+
+signals:
+    void success();
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(RenameFileRequest)
+};
+
+class MoveFileRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    MoveFileRequest(const Account &account,
+                    const QString &repo_id,
+                    const QString &path,
+                    const QString &new_repo_id,
+                    const QString &new_path);
+
+signals:
+    void success();
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(MoveFileRequest)
+};
+
+class RemoveFileRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    RemoveFileRequest(const Account &account, const QString &repo_id,
+                      const QString &path);
+
+signals:
+    void success();
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(RemoveFileRequest)
+};
+
+class StarFileRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    StarFileRequest(const Account &account, const QString &repo_id,
+                    const QString &path);
+
+signals:
+    void success();
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(StarFileRequest)
+};
+
+class UnstarFileRequest : public SeafileApiRequest {
+    Q_OBJECT
+public:
+    UnstarFileRequest(const Account &account, const QString &repo_id,
+                      const QString &path);
+
+signals:
+    void success();
+
+protected slots:
+    void requestSuccess(QNetworkReply& reply);
+
+private:
+    Q_DISABLE_COPY(UnstarFileRequest)
 };
 
 #endif  // SEAFILE_CLIENT_FILE_BROWSER_REQUESTS_H

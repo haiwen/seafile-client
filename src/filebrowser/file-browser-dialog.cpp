e7b68a0e0d96890c732616ba84aabeb05d6faa40
Chilledheart <rwindz0@gmail.com>
2014-11-06 10:53:23 +0800

    refine the words of confirmation
    discussion: https://github.com/haiwen/seafile-client/pull/258#discussion_r19922023

--- src/filebrowser/file-browser-dialog.cpp
+++ src/filebrowser/file-browser-dialog.cpp
@@ -512,8 +512,9 @@ void FileBrowserDialog::onGetDirentRename(const SeafDirent& dirent,
 
 void FileBrowserDialog::onGetDirentRemove(const SeafDirent& dirent)
 {
-    if (seafApplet->yesOrNoBox(tr("Confirm to remove file %1").arg(dirent.name),
-                               this, false))
+    if (seafApplet->yesOrNoBox(dirent.isFile() ?
+            tr("Confirm to remove file \"%1\"").arg(dirent.name) :
+            tr("Confirm to remove folder \"%1\"").arg(dirent.name), this, false))
         data_mgr_->removeDirent(repo_.id, current_path_ + dirent.name,
                                 dirent.isFile());
 }

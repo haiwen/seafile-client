2fad217eb38ea4ca765bb21e0bbc1a6e443c6735
Chilledheart <rwindz0@gmail.com>
2014-12-22 16:53:52 +0800

    enable ci build on osx

---
+++ scripts/ci-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+set -e
+PWD=$(dirname "${BASH_SOURCE[0]}")
+
+export CFLAGS="-Wall -Wextra -Wsign-compare -Wno-long-long -Wno-unused-parameter"
+export CXXFLAGS="-Woverloaded-virtual $CFLAGS"
+
+pushd $PWD/..
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    cmake -DBUILD_TESTING=on .
+    make -j8 VERBOSE=1
+    make test VERBOSE=1
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    export PATH=/usr/local/opt/openssl/bin:/usr/local/opt/curl/bin:$PATH
+    export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/opt/curl/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/sqlite/lib/pkgconfig
+    export CPPFLAGS="-I/usr/local/opt/openssl/include -I/usr/local/opt/sqlite/include -I/usr/local/opt/curl/include"
+    export CXXFLAGS="$CXXFLAGS $CPPFLAGS -I/usr/local/include"
+    export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/opt/sqlite/lib -L/usr/local/opt/curl/lib -L/usr/local/lib"
+    export PKG_CONFIG_LIBDIR=/usr/lib/pkgconfig:/usr/local/Library/ENV/pkgconfig/10.9
+    export ACLOCAL_PATH=/usr/local/share/aclocal
+    cmake -G Xcode -DBUILD_TESTING=on .
+    xcodebuild -configuration Debug -target ALL_BUILD -jobs 8
+    xcodebuild -configuration Debug -target RUN_TESTS
+else
+    printf "not supported platform"
+    exit -1
+fi
+
+popd
---
+++ scripts/install-deps-linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+sudo apt-get update -qq
+sudo apt-get install -qq valac uuid-dev libevent-dev libjansson-dev libqt4-dev
+
+git clone --depth=1 --branch=master git://github.com/haiwen/libsearpc.git deps/libsearpc
+git clone --depth=1 --branch=master git://github.com/haiwen/ccnet.git deps/ccnet
+git clone --depth=1 --branch=master git://github.com/haiwen/seafile.git deps/seafile
+pushd deps/libsearpc
+./autogen.sh && ./configure
+make -j8 && sudo make install
+popd
+
+pushd deps/ccnet
+./autogen.sh && ./configure --enable-client --disable-server
+make -j8 && sudo make install
+popd
+
+pushd deps/seafile
+./autogen.sh && ./configure --disable-fuse --disable-server --enable-client
+make -j8 && sudo make install
+popd

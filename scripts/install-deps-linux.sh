56d3b0a9d3e0fc4cdfbaf7f9427bd988d0ef64fb
Chilledheart <rwindz0@gmail.com>
2015-01-22 13:37:14 +0800

    qt5: build under c++11 mode
    if we are building against qt5, build under c++11 mode for binary consistence.
    this requires your compiler newer than gcc 4.8.1 or clang 3.3.
    and, currently, it is guarenteed under osx.
    
    from qt5.4.0's changelog https://qt.gitorious.org/qt/qtbase/source/64c6bfd1f45f532ea32de49431b66e03e7593f01:dist/changes-5.4.0#L443-449
    "The Qt binary packages (for OS X) are now configured with C++11 enabled."

--- scripts/install-deps-linux.sh
+++ scripts/install-deps-linux.sh
@@ -4,9 +4,13 @@ set -e
 
 sudo add-apt-repository -y ppa:smspillaz/cmake-2.8.12
 sudo add-apt-repository -y ppa:beineri/opt-qt54
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
+sudo apt-get install -y gcc-4.8 g++-4.8
 sudo apt-get install -y valac uuid-dev libevent-dev re2c libjansson-dev cmake cmake-data libqt4-dev
 sudo apt-get install -y qt54base qt54translations qt54tools qt54webkit
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 
 git clone --depth=1 --branch=master git://github.com/haiwen/libsearpc.git deps/libsearpc
 git clone --depth=1 --branch=master git://github.com/haiwen/ccnet.git deps/ccnet

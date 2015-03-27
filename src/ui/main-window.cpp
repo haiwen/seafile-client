b3a2269568979267c9b79384fc172ce87f3cd029
Chilledheart <rwindz0@gmail.com>
2015-03-27 17:11:57 +0800

    don't stay outside screens
    if we have mutiple screens

--- src/ui/main-window.cpp
+++ src/ui/main-window.cpp
@@ -64,6 +64,24 @@ void showTestDialog(QWidget *parent)
 
 } // namespace
 
+/// a little helper function to detect if the pos is outside the screens
+static bool isOutsideScreens(const QPoint &pos) {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
+    QList<QScreen*> screens = QGuiApplication::screens();
+    for (int i = 0; i < screens.size(); ++i) {
+        if (screens[i]->geometry().contains(pos))
+            return false;
+    }
+#else
+    QDesktopWidget *desktop = QApplication::desktop();
+    for (int i = 0; i < desktop->numScreens(); ++i) {
+        if (desktop->screenGeometry(i).contains(pos))
+            return false;
+    }
+#endif
+    return true;
+}
+
 
 MainWindow::MainWindow()
 {
@@ -244,6 +262,10 @@ void MainWindow::readSettings()
     } else {
         pos = settings.value("pos", getDefaultPosition()).toPoint();
         size = settings.value("size", QSize()).toSize();
+
+        // we don't want to be out of screen
+        if (isOutsideScreens(pos))
+            pos = getDefaultPosition();
     }
 
     first_show = false;

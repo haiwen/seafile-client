8ddce0b84aa42239e2bd94d9ec70b07ca87ac8a9
Shuai Lin <linshuai2012@gmail.com>
2015-07-07 11:59:57 +0800

    [ext] remove debug logs

--- extensions/commands.cpp
+++ extensions/commands.cpp
@@ -132,9 +132,9 @@ bool GetFileStatusCommand::parseResponse(const std::string& raw_resp,
         *status = RepoInfo::NoStatus;
     }
 
-    seaf_ext_log ("[GetFileStatusCommand] status for %s is %s, raw_resp is %s\n",
-                  path_in_repo_.c_str(),
-                  seafile::toString(*status).c_str(), raw_resp.c_str());
+    // seaf_ext_log ("[GetFileStatusCommand] status for %s is %s, raw_resp is %s\n",
+    //               path_in_repo_.c_str(),
+    //               seafile::toString(*status).c_str(), raw_resp.c_str());
 
     return true;
 }

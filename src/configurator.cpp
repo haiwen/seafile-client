3bf81aab22f6d64dc0db03241a595c5411578a3c
Blindauer Emmanuel <e.blindauer@gmail.com>
2015-07-09 06:30:35 +0200

    Use HKLM for PreconfigureDirectory entry

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -34,7 +34,7 @@ const char *kMyComputerNamespacePath =
 const char* const kPreconfigureDirectory = "PreconfigureDirectory";
 QString getPreconfigureDirectory()
 {
-    RegElement reg(HKEY_CURRENT_USER, "SOFTWARE\\Seafile", kPreconfigureDirectory, "");
+    RegElement reg(HKEY_LOCAL_MACHINE, "SOFTWARE\\Seafile", kPreconfigureDirectory, "");
     if (!reg.exists()) {
         return QString();
     }

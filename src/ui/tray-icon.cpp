ea06e7fc5b736266a3318e0d2f5c92766f554d80
Lingxiang Pan <13552386672@139.com>
2017-06-16 18:05:22 +0800

    Disabled show_sync_errors menu item in tray icon when there is no sync error. (#925)
    * Disabled show_sync_errors menu item in tray icon when there is no sync error.
    
    * Check the return type of getSyncErrors().

--- src/ui/tray-icon.cpp
+++ src/ui/tray-icon.cpp
@@ -230,6 +230,19 @@ void SeafileTrayIcon::prepareContextMenu()
         enable_auto_sync_action_->setVisible(true);
         disable_auto_sync_action_->setVisible(false);
     }
+
+    std::vector<SyncError> errors;
+    bool success = seafApplet->rpcClient()->getSyncErrors(&errors, 0, 1);
+    if (!success) {
+        qDebug("failed to get sync errors");
+        return;
+    }
+
+    if (errors.empty()) {
+        show_sync_errors_action_->setEnabled(false);
+    } else {
+        show_sync_errors_action_->setEnabled(true);
+    }
 }
 
 void SeafileTrayIcon::createGlobalMenuBar()

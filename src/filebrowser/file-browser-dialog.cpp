8b05747e0ef90e22613eeb342cea2100fef1ee5f
YuZhao Sun <yuzhao.sun@seafile.com>
2017-10-31 00:46:58 +0800

    Improve the number of display files

--- src/filebrowser/file-browser-dialog.cpp
+++ src/filebrowser/file-browser-dialog.cpp
@@ -827,6 +827,7 @@ void FileBrowserDialog::onUploadFinished(bool success)
         const SeafDirent dir = SeafDirent::dir(task->name());
         // TODO: insert the Item prior to the item where uploading occurs
         table_model_->insertItem(0, dir);
+	data_mgr_->getDirentsFromServer(repo_.id, current_path_);
         return;
     }
 
@@ -859,6 +860,7 @@ void FileBrowserDialog::onUploadFinished(bool success)
         else
             table_model_->replaceItem(name, dirent);
     }
+    data_mgr_->getDirentsFromServer(repo_.id, current_path_);
 }
 
 bool FileBrowserDialog::setPasswordAndRetry(FileNetworkTask *task)
@@ -935,6 +937,7 @@ void FileBrowserDialog::updateTable(const QList<SeafDirent>& dirents)
         upload_button_->setEnabled(true);
     }
     gohome_action_->setEnabled(current_path_ != "/");
+    details_label_->setText(tr("%1 items").arg(dirents.size()));
 }
 
 void FileBrowserDialog::chooseFileToUpload()
@@ -1050,6 +1053,7 @@ void FileBrowserDialog::onDirectoryCreateSuccess(const QString &path)
     const SeafDirent dirent = SeafDirent::dir(name);
     // TODO insert to the pos where the drop event triggered
     table_model_->insertItem(0, dirent);
+    data_mgr_->getDirentsFromServer(repo_.id, current_path_);
 }
 
 void FileBrowserDialog::onDirectoryCreateFailed(const ApiError&error)
@@ -1116,6 +1120,7 @@ void FileBrowserDialog::onDirentRemoveSuccess(const QString& path)
     if (::pathJoin(current_path_, name) != path)
         return;
     table_model_->removeItemNamed(name);
+    data_mgr_->getDirentsFromServer(repo_.id, current_path_);
 }
 
 void FileBrowserDialog::onDirentRemoveFailed(const ApiError&error)
@@ -1132,6 +1137,7 @@ void FileBrowserDialog::onDirentsRemoveSuccess(const QString& parent_path,
     foreach (const QString& name, filenames) {
         // printf("removed file: %s\n", name.toUtf8().data());
         table_model_->removeItemNamed(name);
+	data_mgr_->getDirentsFromServer(repo_.id, current_path_);
     }
 }
 

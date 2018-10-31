23d7d168c6f930515bbec4aab8e70592b1b75ea3
sunyuzhao <32261840+sunyuzhao@users.noreply.github.com>
2018-10-31 12:25:56 +0800

    username => accountInfo name (#1143)
    * username => accountInfo name
    
    * fix code
    
    * fix code

--- src/ui/account-view.cpp
+++ src/ui/account-view.cpp
@@ -166,7 +166,9 @@ void AccountView::onAccountChanged()
     if (!accounts.empty()) {
         for (size_t i = 0, n = accounts.size(); i < n; i++) {
             const Account &account = accounts[i];
-            QString text = account.username + "(" + account.serverUrl.host() + ")";
+            QString text_name = account.accountInfo.name.isEmpty() ?
+                        account.username : account.accountInfo.name;
+            QString text = text_name + "(" + account.serverUrl.host() + ")";
             if (!account.isValid()) {
                 text += ", " + tr("not logged in");
             }

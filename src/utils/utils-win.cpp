ca84c8e0575d5559a185ac0a2cd6631c8205b25d
Shuai Lin <linshuai2012@gmail.com>
2018-12-25 18:09:23 +0800

    win: base64 encode the username before appending to the pipe name

--- src/utils/utils-win.cpp
+++ src/utils/utils-win.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <wincrypt.h>
 #include <glib.h>
 
 #include <QLibrary>
@@ -277,6 +278,14 @@ bool fixQtHDPINonIntegerScaling()
     return true;
 }
 
+char *b64encode(const char *input)
+{
+    char buf[32767] = {0};
+    DWORD retlen = 32767;
+    CryptBinaryToString((BYTE*) input, strlen(input), CRYPT_STRING_BASE64 | CRYPT_STRING_NOCRLF, buf, &retlen);
+    return strdup(buf);
+}
+
 std::string getLocalPipeName(const char *pipe_name)
 {
     DWORD buf_char_count = 32767;
@@ -289,7 +298,9 @@ std::string getLocalPipeName(const char *pipe_name)
     }
     else {
         std::string ret(pipe_name);
-        ret += user_name_buf;
+        char *encoded = b64encode(user_name_buf);
+        ret += encoded;
+        free(encoded);
         return ret;
     }
 }

519275ab064e3e5505da5a1ea11293e18413bd48
sun20121818 <1184550842@qq.com>
2020-09-04 15:51:39 +0800

    Support using system api to detect network status (#1316)
    * Support using system api to detect network status
    
    * Synchronously block calling the NotifyRouteChange function
    
    * Support ipv6
    
    * Change after review

---
+++ src/utils/monitor-netstat.cpp
@@ -0,0 +1,14 @@
+#include <QFile>
+
+#include "utils/monitor-netstat.h"
+#include "utils/utils-win.h"
+#include "api/api-client.h"
+
+SINGLETON_IMPL(MonitorNetStatWorker)
+MonitorNetStatWorker::MonitorNetStatWorker() {
+
+}
+
+MonitorNetStatWorker::~MonitorNetStatWorker() {
+
+}

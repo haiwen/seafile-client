f40c863f0720d1c810a7f4395f2823800af16a8e
sun <1184550842@qq.com>
2021-06-24 11:09:19 +0800

    Update i18n

--- i18n/seafile_et_EE.ts
+++ i18n/seafile_et_EE.ts
@@ -1562,6 +1562,10 @@ File path contains invalid characters. It is not synced to this computer.</sourc
         <source>The file path %1 contains symbols that are not supported by the Windows system.</source>
         <translation type="unfinished"/>
     </message>
+    <message>
+        <source>Library is too large to sync.</source>
+        <translation type="unfinished"/>
+    </message>
 </context>
 <context>
     <name>PrivateShareDialog</name>
@@ -2178,6 +2182,10 @@ File path contains invalid characters. It is not synced to this computer.</sourc
     </message>
     <message>
         <source>The file path contains symbols that are not supported by the Windows system</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Library is too large to sync.</source>
         <translation type="unfinished"/>
     </message>
 </context>
@@ -2951,6 +2959,10 @@ File path contains invalid characters. It is not synced to this computer.</sourc
     </message>
     <message>
         <source>Cancel</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Hide Windows incompatibility paths Message</source>
         <translation type="unfinished"/>
     </message>
 </context>

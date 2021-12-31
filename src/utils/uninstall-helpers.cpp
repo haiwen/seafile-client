50775a231f2da48fc0915a7f9cd3172ec1edfa82
Chilledheart <rwindz0@gmail.com>
2021-12-31 11:17:15 +0800

    win: improve delete_dir_recursively with FOF_NO_UI

--- src/utils/uninstall-helpers.cpp
+++ src/utils/uninstall-helpers.cpp
@@ -128,25 +128,20 @@ int delete_dir_recursively(const QString& path_in)
 
     int len = path.length();
 
-    wchar_t *wpath = new wchar_t[len + 2];
+    std::unique_ptr<wchar_t[]> wpath(new wchar_t[len + 2]);
 
-    wcscpy(wpath, path.toStdWString().c_str());
+    wcscpy(wpath.get(), path.toStdWString().c_str());
     wpath[len + 1] = L'\0';
 
-    SHFILEOPSTRUCTW fileop;
-    fileop.hwnd   = NULL;       // no status display
-    fileop.wFunc  = FO_DELETE;  // delete operation
-    fileop.pFrom  = wpath; // source file name as double null terminated string
-    fileop.pTo    = NULL;         // no destination needed
-    fileop.fFlags = FOF_NOCONFIRMATION|FOF_SILENT; // do not prompt the user
+    SHFILEOPSTRUCTW fileop {};
+    fileop.wFunc  = FO_DELETE;   // delete operation
+    fileop.pFrom  = wpath.get(); // source file name as double null terminated string
+    /* This is equivalent to FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI |
+     * FOF_NOCONFIRMMKDIR. see https://docs.microsoft.com/en-us/windows/win32/api/shellapi/ns-shellapi-shfileopstructw */
+    fileop.fFlags = FOF_NO_UI;   // do not prompt the user
 
-    fileop.fAnyOperationsAborted = FALSE;
-    fileop.lpszProgressTitle     = NULL;
-    fileop.hNameMappings         = NULL;
-
+    /* TODO: This function has been replaced in Windows Vista by IFileOperation. */
     int ret = SHFileOperationW(&fileop);
-
-    delete []wpath;
 
     if (ret == 0) {
         return 0;

f08f87f65f2c05223f0ed54e63ea847def3d39c5
rumtid <liwei.jiang@seafile.com>
2022-12-22 15:57:44 +0800

    Enrich the notification messages

--- src/message-poller.cpp
+++ src/message-poller.cpp
@@ -217,7 +217,15 @@ void MessagePoller::processNotification(const SyncNotification& notification)
         }
 
         QString msg = tr("Confirm to bulk delete files in library \"%1\" ?")
-                        .arg(doc["repo_name"].toString().trimmed());
+                          .arg(doc["repo_name"].toString().trimmed());
+
+        QRegularExpression re("Deleted \"(.+)\" and (.+) more files.");
+        auto match = re.match(doc["delete_files"].toString().trimmed());
+        if (match.hasMatch()) {
+            msg += tr("\n(deleted \"%1\" and %2 more files.)")
+                       .arg(match.captured(1)).arg(match.captured(2));
+        }
+
         if (seafApplet->yesOrCancelBox(msg, nullptr, false)) {
             rpc_client_->addDelConfirmation(doc["confirmation_id"].toString(), false);
         } else {

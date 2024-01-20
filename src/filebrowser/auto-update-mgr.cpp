dcc7a00ebfb7414dfc87c9563873f8cf380bdf21
rumtid <liwei.jiang@seafile.com>
2024-01-20 15:03:13 +0800

    Support file limit exceeded errors (#1507)

--- src/filebrowser/auto-update-mgr.cpp
+++ src/filebrowser/auto-update-mgr.cpp
@@ -235,6 +235,8 @@ void AutoUpdateManager::onUpdateTaskFinished(bool success)
             error_msg = tr("Authorization expired");
         } else if (task->httpErrorCode() == 441) {
             error_msg = tr("File does not exist");
+        } else if (task->httpErrorCode() == 442) {
+            error_msg = tr("File count limit exceeded");
         } else {
             error_msg = task->errorString();
         }

7e7b5e149186a7534795c9c9aac2a747c15fbe49
yangheran <heran.yang@seafile.com>
2024-12-21 14:54:23 +0800

    Adjust sync error message

--- src/utils/seafile-error.cpp
+++ src/utils/seafile-error.cpp
@@ -135,33 +135,33 @@ QString translateFileSyncErrorCode(const int error_code)
     QString error_str;
     switch (error_code) {
         case SYNC_ERROR_ID_FILE_LOCKED_BY_APP:
-            error_str = QObject::tr("File is locked by another application. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("File is locked by another application. Right click and choose \"Show sync errors\" to find details");
             break;
         case SYNC_ERROR_ID_INDEX_ERROR:
-            error_str = QObject::tr("Error when indexing. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("Error when indexing. Right click and choose \"Show sync errors\" to find details");
             break;
         case SYNC_ERROR_ID_PATH_END_SPACE_PERIOD:
-            error_str = QObject::tr("Path ends with space or period character. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("Path ends with space or period character. Right click and choose \"Show sync errors\" to find details");
             break;
         case SYNC_ERROR_ID_PATH_INVALID_CHARACTER:
-            error_str = QObject::tr("Path contains invalid characters like '|' or ':'. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("Path contains invalid characters like '|' or ':'. Right click and choose \"Show sync errors\" to find details");
             break;
         case SYNC_ERROR_ID_UPDATE_TO_READ_ONLY_REPO:
-            error_str = QObject::tr("Created or updated a file in a non-writable library or folder. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("Created or updated a file in a non-writable library or folder. Right click and choose \"Show sync errors\" to find details");
             break;
         case SYNC_ERROR_ID_REMOVE_UNCOMMITTED_FOLDER:
-            error_str = QObject::tr("A folder that may contain not-yet-uploaded files is moved to seafile-recycle-bin folder. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("A folder that may contain not-yet-uploaded files is moved to seafile-recycle-bin folder. Right click and choose \"Show sync errors\" to find details");
             break;
 #if !defined(Q_OS_WIN32)
         case SYNC_ERROR_ID_INVALID_PATH_ON_WINDOWS:
-            error_str = QObject::tr("The file path contains symbols that are not supported by the Windows system. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("The file path contains symbols that are not supported by the Windows system. Right click and choose \"Show sync errors\" to find details");
             break;
 #endif
         case SYNC_ERROR_ID_CHECKOUT_FILE:
-            error_str = QObject::tr("Failed to download file. Please check disk space or folder permissions. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("Failed to download file. Please check disk space or folder permissions. Right click and choose \"Show sync errors\" to find details");
             break;
         case SYNC_ERROR_ID_CASE_CONFLICT:
-            error_str = QObject::tr("Path has character case conflict with existing file or folder. Will not be downloaded. Please check \"File sync errors\" for details");
+            error_str = QObject::tr("Path has character case conflict with existing file or folder. Will not be downloaded. Right click and choose \"Show sync errors\" to find details");
             break;
         default:
             return translateSyncErrorCode(error_code);

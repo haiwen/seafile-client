d64501c8a9c62c4fe846ec9cf303952b9a8eb0be
rumtid <liwei.jiang@seafile.com>
2025-05-21 11:47:16 +0800

    Add a FAQ link to error dialog (#1618)
    * Add a FAQ link to error dialog
    
    * Improve the FAQ link
    
    * Update descriptions

--- src/ui/sync-errors-dialog.cpp
+++ src/ui/sync-errors-dialog.cpp
@@ -95,7 +95,20 @@ SyncErrorsDialog::SyncErrorsDialog(QWidget *parent)
     stack_->insertWidget(INDEX_TABE_VIEW, table_);
     stack_->setContentsMargins(0, 0, 0, 0);
 
+    QString text;
+    if (QLocale::system().name() == "zh_CN") {
+        text = "<a href=\"https://cloud.seafile.com/wiki/publish/seafile-user-manual/7he2/\">" + tr("Descriptions for sync errors") + "</a>";
+    } else {
+        text = "<a href=\"https://help.seafile.com/faq/#meaning-file-syncing-errors\">" + tr("Descriptions for sync errors") + "</a>";
+    }
+    QLabel *label = new QLabel(text);
+    label->setTextFormat(Qt::RichText);
+    label->setMargin(6);
+
+    connect(label, SIGNAL(linkActivated(QString)), this, SLOT(onLinkActivated(QString)));
+
     vlayout->addWidget(stack_);
+    vlayout->addWidget(label);
 
     onModelReset();
     connect(model_, SIGNAL(modelReset()), this, SLOT(onModelReset()));
@@ -128,6 +141,10 @@ void SyncErrorsDialog::onModelReset()
     }
 }
 
+void SyncErrorsDialog::onLinkActivated(const QString& link)
+{
+    openUrl(QUrl(link));
+}
 
 void SyncErrorsDialog::createEmptyView()
 {
--- src/ui/sync-errors-dialog.h
+++ src/ui/sync-errors-dialog.h
@@ -33,6 +33,7 @@ public:
 
 private slots:
     void onModelReset();
+    void onLinkActivated(const QString& link);
 
 private:
     void createEmptyView();

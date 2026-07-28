7a0f5baba1bd8f607d8a3251440097fa9841807e
rumtid <3798047+rumtid@users.noreply.github.com>
2026-07-28 10:53:00 +0800

    Fix the UI style of Sync Errors Dialog

--- src/ui/sync-errors-dialog.cpp
+++ src/ui/sync-errors-dialog.cpp
@@ -94,7 +94,7 @@ SyncErrorsDialog::SyncErrorsDialog(QWidget *parent)
     stack_ = new QStackedWidget;
     stack_->insertWidget(INDEX_EMPTY_VIEW, empty_view_);
     stack_->insertWidget(INDEX_TABE_VIEW, table_);
-    stack_->setContentsMargins(0, 0, 0, 0);
+    stack_->setContentsMargins(20, 20, 20, 0);
 
     QString text;
     if (I18NHelper::getInstance()->isChinese()) {
